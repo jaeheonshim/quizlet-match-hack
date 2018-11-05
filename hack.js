e374189fbd9d21c7bdbeadbfb942c83151cd774c
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2018-11-05 12:20:59 -0500

    Create hack.js

---
+++ hack.js
@@ -0,0 +1,2 @@
+document.getElementsByClassName("UIButton UIButton--hero")[0].click()
+setTimeout(function(){for(var F = setTimeout(";"), i = 0; i < F; i++) clearTimeout(i)}, 5100); //Change 5100 to desired time in milliseconds
