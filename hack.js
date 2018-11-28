7e6f25a86128e2e73e95c07362677e6f22b901d0
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2018-11-28 06:01:54 -0800

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,2 +1,1 @@
-document.getElementsByClassName("UIButton UIButton--hero")[0].click()
-setTimeout(function(){for(var F = setTimeout(";"), i = 0; i < F; i++) clearTimeout(i)}, 5100); //Change 5100 to desired time in milliseconds
+alert("Lol ur stupid");
