3024809a04a37dbea1c6ea74e2f08fa6094ac575
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2018-11-28 10:07:12 -0500

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,1 +1,2 @@
-alert("Lol ur stupid");
+document.getElementsByClassName("UIButton UIButton--hero")[0].click()
+setTimeout(function(){for(var F = setTimeout(";"), i = 0; i < F; i++) clearTimeout(i)}, 5100); //Change 5100
