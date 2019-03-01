b1edb2c971527ddc753029920c924a35054bf6d7
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2019-03-01 10:06:29 -0500

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,2 +1,2 @@
-document.getElementsByClassName("UIButton UIButton--hero")[0].click()
+document.getElementsByClassName("UIButton UIButton--hero")[0].click();
 setTimeout(function(){for(var F = setTimeout(";"), i = 0; i < F; i++) clearTimeout(i)}, 5100); //Change 5100
